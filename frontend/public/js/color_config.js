aec13b1575bd4846c1e74e61fda10338fb8ac9cd
nirmal_prajapati <nirmalprajapati727@gmail.com>
2025-02-28 22:37:47 +0530

    ezservice

---
+++ frontend/public/js/color_config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#6c63ff',
+                secondary: '#6c63f',
+            }
+        }
+    }
+}
