a16a8545d35bc89f6509a5b1ce947cd3b561f027
Vinayrk2 <v277772@gmail.com>
2025-03-26 23:02:17 -0700

    added all staticfiles

---
+++ staticpages/public/js/color_config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#6c63ff',
+                secondary: '#9892fc',
+            }
+        }
+    }
+}
