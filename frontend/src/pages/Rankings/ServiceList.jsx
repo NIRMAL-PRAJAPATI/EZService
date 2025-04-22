66a2de20475a5fc8bb9851f4e17e304a15660928
Neesargsoni <neesargs@gmail.com>
2025-04-21 18:22:45 -0600

    changes in rankings page

---
+++ frontend/src/pages/Rankings/ServiceList.jsx
@@ -0,0 +1,24 @@
+"use client"
+
+import { IoClose } from "react-icons/io5"
+
+const ServiceList = ({ isOpen, onClose }) => {
+  const services = ["Home Cleaning", "Trainer", "Plumbing", "Mechanic", "Education", "Laboure", "Beauty & Wellness"]
+
+  return (
+    <section className={`absolute bg-black/30 z-10 w-full h-full ${isOpen ? "" : "hidden"}`}>
+      <div className="bg-primary text-white h-full w-3/4 md:w-1/4 ml-auto p-2 border overflow-y-scroll">
+        <IoClose className="ml-auto text-white cursor-pointer text-xl" onClick={onClose} />
+        <ul className="space-y-1 tracking-wider pl-2">
+          {services.map((service, index) => (
+            <li key={index} className="border-b border-primary hover:border-gray-300 p-1 cursor-pointer">
+              {service}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default ServiceList
