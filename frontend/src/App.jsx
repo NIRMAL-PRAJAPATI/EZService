ea23ddb0e81293b1d4c5d74c6478f4462ceca43e
nirmal_prajapati <nirmalprajapati727@gmail.com>
2025-04-22 13:15:11 +0530

    jnj

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
-<<<<<<< HEAD
 import { lazy, Suspense } from 'react'
 const UserHome = lazy(() => import('./pages/UserHome'));
 const Footer = lazy(() => import('./components/Footer'));
@@ -13,16 +12,6 @@ const Complaint = lazy(() => import('./pages/Complaint'));
 const Order = lazy(() => import('./pages/Order'));
 import Loading from './components/Loading'
 const ErrorPage = lazy(() => import('./pages/ErrorPage'));
-=======
-import UserHome from './pages/UserHome'
-import Footer from './components/Footer'
-import ProfilePage from './pages/Pofile'
-import ServicePage from './pages/ServicesPage'
-import About from './pages/About'
-import Explore from './pages/Explore'
-import Complaint from './pages/Complaint'
-import Rankings from './pages/Rankings/Rankings'
->>>>>>> 66a2de20475a5fc8bb9851f4e17e304a15660928
 
 function App() {
   return (
@@ -37,12 +26,8 @@ function App() {
           <Route path="/services" element={<ServicePage />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/complaint" element={<Complaint />} />
-<<<<<<< HEAD
           <Route path="/order" element={<Order />} />
           <Route path="/errorpage" element={<ErrorPage />} />
-=======
-          <Route path="/rankings" element={<Rankings />} />
->>>>>>> 66a2de20475a5fc8bb9851f4e17e304a15660928
         </Routes>
         </Suspense>
         <Footer/>
