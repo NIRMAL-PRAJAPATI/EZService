7c4a90a059d415c1295774a6ca0c8064cbd482fd
nirmal_prajapati <nirmalprajapati727@gmail.com>
2025-04-26 19:03:41 +0530

    h

--- backend/app.js
+++ backend/app.js
@@ -12,11 +12,8 @@ app.use(require("./middlewares/pagination"))
 // Routes
 app.use("/customer",require("./routes/customerInfoRoutes"));
 app.use("/category",require("./routes/serviceCategoryRoutes"));
-<<<<<<< HEAD
 app.use("/services", require("./routes/serviceRoutes"));
-=======
 app.use("/template",require("./routes/categoryTemplateRoutes"))
->>>>>>> b0a8057302fef4acbbb604bb5fac0732aa9d6477
 
 app.listen(3000, () => {
     console.log("Server is running on port http://localhost:3000/");
