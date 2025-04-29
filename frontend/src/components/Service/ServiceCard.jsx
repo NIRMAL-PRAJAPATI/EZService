f41a9dd0f9c209c9996e49a11e823d4a93a8ef9f
Neesargsoni <neesargs@gmail.com>
2025-04-29 00:14:25 -0600

    added route on all services card

--- frontend/src/components/Service/ServiceCard.jsx
+++ frontend/src/components/Service/ServiceCard.jsx
@@ -1,47 +1,53 @@
 import { AlignLeft, Search, SlidersHorizontal, Star, XIcon } from 'lucide-react'
+import { useNavigate } from 'react-router-dom'
 
 const ServiceCard = ({services})=>{
-
+    const navigate = useNavigate()
 
     return (
         <>
         {
             services?.length > 0 && services.map((service)=>{
-
-            return <a href="/frontend/views/user/service_profile.html" className="bg-white overflow-hidden rounded-md border border-gray-200 hover:border-indigo-500">
-                <div className="items-start">
-                  <div className="w-full bg-gray-100 h-26">
-                    <img
-                      src="https://c8.alamy.com/comp/DERFBR/colourful-indian-shop-in-puttaparthi-andhra-pradesh-india-DERFBR.jpg"
-                      alt="Provider"
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
-                  <div className="flex-1 pb-3 px-1 sm:px-2">
-                    {/* <span className="text-green-500 bg-gray-100 font-semibold rounded-full px-2 text-[10px] ml-1">
-                      Available
-                    </span> */}
-                    <h3 className="font-semibold text-md leading-none py-1" title='Ananta Plumbing Service'>
-                      {service.name}
-                    </h3>
-                    <p className="text-sm text-gray-600 mb-1 font-semibold">
-                      ₹{service.visiting_charge}
-                    </p>
-                    <div className="flex items-center text-[12px]">
-                      <span className="text-gray-500">
-                        {service.city}, {service.state}, {service.country}
-                      </span>
+            return (
+                <div 
+                    key={service.id}
+                    onClick={() => navigate('/service/2')}
+                    className="bg-white overflow-hidden rounded-md border border-gray-200 hover:border-indigo-500 cursor-pointer"
+                >
+                    <div className="items-start">
+                      <div className="w-full bg-gray-100 h-26">
+                        <img
+                          src="https://c8.alamy.com/comp/DERFBR/colourful-indian-shop-in-puttaparthi-andhra-pradesh-india-DERFBR.jpg"
+                          alt="Provider"
+                          className="w-full h-full object-cover"
+                        />
+                      </div>
+                      <div className="flex-1 pb-3 px-1 sm:px-2">
+                        {/* <span className="text-green-500 bg-gray-100 font-semibold rounded-full px-2 text-[10px] ml-1">
+                          Available
+                        </span> */}
+                        <h3 className="font-semibold text-md leading-none py-1" title='Ananta Plumbing Service'>
+                          {service.name}
+                        </h3>
+                        <p className="text-sm text-gray-600 mb-1 font-semibold">
+                          ₹{service.visiting_charge}
+                        </p>
+                        <div className="flex items-center text-[12px]">
+                          <span className="text-gray-500">
+                            {service.city}, {service.state}, {service.country}
+                          </span>
+                        </div>
+                        <div className="flex items-center text-indigo-500 gap-0.5 mt-1">
+                          <Star className="h-3 w-3 fill-indigo-500 inline-block" />
+                          <Star className="h-3 w-3 fill-indigo-500 inline-block" />
+                          <Star className="h-3 w-3 fill-indigo-500 inline-block" />
+                          <Star className="h-3 w-3 fill-indigo-500 inline-block" />
+                          <Star className="h-3 w-3 inline-block" />
+                        </div>
+                      </div>
                     </div>
-                    <div className="flex items-center text-indigo-500 gap-0.5 mt-1">
-                      <Star className="h-3 w-3 fill-indigo-500 inline-block" />
-                      <Star className="h-3 w-3 fill-indigo-500 inline-block" />
-                      <Star className="h-3 w-3 fill-indigo-500 inline-block" />
-                      <Star className="h-3 w-3 fill-indigo-500 inline-block" />
-                      <Star className="h-3 w-3 inline-block" />
-                    </div>
-                  </div>
                 </div>
-              </a>
+            )
             })
         }
         </>
