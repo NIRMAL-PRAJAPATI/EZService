f2779cfab14c93475a8c83fa8ebd120de3aae96b
nirmal_prajapati <nirmalprajapati727@gmail.com>
2025-04-29 12:38:19 +0530

    registratio things change

--- frontend/src/pages/MobileVarification.jsx
+++ frontend/src/pages/MobileVarification.jsx
@@ -1,22 +1,44 @@
-import { useSearchParams } from "react-router-dom";
+import axios from "axios";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
  function MobileVarification() {
+  const { handleSubmit } = useForm();
 
-  const [searchParams] = useSearchParams();
-  const mobile = searchParams.get('mobile');
+  const location = useLocation();
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const { formData } = location.state || {};
+  console.log(formData);
+  const navigate = useNavigate();
+
+  const api = "http://localhost:3000";
+
+  const onRegister = async () => {
+    await axios.post(`${api}/customer/register`, formData).then(response => {
+      navigate('/');
+    }).catch((error) => {
+      if(error.response) {
+        setErrorMessage(error.response.data.errorMessage);
+      } else {
+        setErrorMessage("");
+      }
+    })
+  }
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-white">
       <div className="w-full max-w-md p-5">
       <h1 className="text-3xl font-extrabold text-center mb-8">Mobile Varification</h1>
 
-<form>
+<form onClick={handleSubmit(onRegister)}>
   <div className="mb-4 relative">
     <label className="absolute left-3 -top-3 bg-white px-1 text-sm font-medium text-indigo-500">Mobile No.</label>
     <input
       type="mobile"
       name="mobile"
-      value={mobile}
+      value={formData.mobile}
       className="block w-full pl-4 pr-3 py-3 text-gray-800 border border-gray-300 rounded-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
     />
   </div>
@@ -31,7 +53,7 @@ import { useSearchParams } from "react-router-dom";
   </div>
   <button type="button" className="border-none text-white rounded-sm h-12 bg-indigo-500">Get OTP</button>
   </div>
-  <p className="text-red-600 -mt-2 mb-1 text-sm"></p>
+  <p className="text-red-600 -mt-2 mb-1 text-sm">{errorMessage}</p>
   <button
     type="submit"
     className="w-full bg-indigo-500 text-white py-3 px-4 rounded-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Verify</button>
