01cf49a3b025b35646d093bdeb43203d4a9308c1
Vinayrk2 <v277772@gmail.com>
2025-06-22 02:07:13 +0530

    build update

---
+++ frontend/dist/assets/HomeAppliance-Bj3y4a3m.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CQANM4Jj.js";import{S as r}from"./Services-CWbCbNxe.js";function p(){return e.jsx("div",{children:e.jsx(r,{serviceType:"appliances"})})}export{p as default};
---
+++ frontend/dist/assets/home-renovation-CgCoozvU.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CQANM4Jj.js";import{S as r}from"./Services-CWbCbNxe.js";function t(){return e.jsx("div",{children:e.jsx(r,{serviceType:"renovation"})})}export{t as default};
