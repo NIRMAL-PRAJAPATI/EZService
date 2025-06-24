809c1e307421857f060b21fff43b1bdad6f8a9d7
nirmal_prajapati <nirmalprajapati727@gmail.com>
2025-06-24 23:17:40 +0530

    adfwvfw

--- frontend/dist/assets/HomeAppliance-Bj3y4a3m.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CQANM4Jj.js";import{S as r}from"./Services-CWbCbNxe.js";function p(){return e.jsx("div",{children:e.jsx(r,{serviceType:"appliances"})})}export{p as default};
--- frontend/dist/assets/Party-1Fc4GnoL.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CQANM4Jj.js";import{S as e}from"./Services-CWbCbNxe.js";function i(){return r.jsx("div",{children:r.jsx(e,{serviceType:"party"})})}export{i as default};
