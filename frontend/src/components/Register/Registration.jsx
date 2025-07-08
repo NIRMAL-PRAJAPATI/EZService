181d60f544040103547864220ea0b5e82373e65f
nirmal_prajapati <nirmalprajapati727@gmail.com>
2025-07-08 19:23:07 +0530

    api resolve

--- frontend/src/components/Register/Registration.jsx
+++ frontend/src/components/Register/Registration.jsx
@@ -2,7 +2,7 @@ import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import api from '../config/axios-config'
+import api from '../../config/axios-config';
 
 function Registration () {
   const { register, handleSubmit, reset, watch,setValue, formState: {errors} } = useForm();
