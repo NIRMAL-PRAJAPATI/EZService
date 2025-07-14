9306be4aa52045988c213498e8cbd32b75f3ea12
nirmal_prajapati <nirmalprajapati727@gmail.com>
2025-07-14 22:55:10 +0530

    done production

--- frontend/src/config/auth-config.js
+++ frontend/src/config/auth-config.js
@@ -3,7 +3,8 @@ import axios from "axios";
 // create axios instance
 const authApi = axios.create(
     {
-        baseURL: import.meta.env.VITE_API_BACKEND_API,
+        // baseURL: import.meta.env.VITE_API_BACKEND_API,
+        baseURL: "http://ec2-43-204-112-76.ap-south-1.compute.amazonaws.com",
         // baseURL: 'http://localhost:3000',
         headers: {
             'Content-Type':'application/json'
